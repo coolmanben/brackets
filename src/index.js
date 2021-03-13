506fd723d3ccab4275bce2f81400db1638047660
coolmanben <mZmiterizrigi@gmail.com>
2021-03-13 15:53:58 +0300

    init: start task
    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  // преобразуем в массив подстрок
+  let bracketsConfigArray = bracketsConfig.map((arr) => arr.join(""));
+
+  var instr = str + '&';
+  var tempstr = "" ;
+  startwhile = true;
+  // проходим по массиву подстрок, до тех пор пока находим подстроки
+  while ( startwhile ) {  
+    startwhile = false;
+    for (var n = 0; n < bracketsConfigArray.length ; n++ ) { 
+      if ( instr.includes( bracketsConfigArray[n] ) ) {
+          tempstr = instr.replace ( bracketsConfigArray[n] , "");
+          instr = tempstr;
+          startwhile = true;
+      }
+    }
+  }
+  if ( instr == '&' ) {
+    return true;
+  }
+  return false;
+};
